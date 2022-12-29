20d759e058089501bb384b82cea8e8b6a8747d90
cleidianeloy <cleidianeloy@hotmail.com>
2022-12-29 11:40:15 -0300

    Adicionando informações vindas da API sobre os casos de covid nos estados brasileiros

--- src/States.js
+++ src/States.js
@@ -1,7 +1,15 @@
 export function States(props){
+    let cases = props.state.cases;
+    let deaths = props.state.deaths;
+    let suspects = props.state.suspects;
     return(
-        <div>
+        <div className="statesInfo">
             <h2>{props.state.state}</h2>
+            <ul>
+                <li>Casos Confirmados: {cases}</li>
+                <li>Mortes: {deaths}</li>
+                <li>Suspeitos: {suspects}</li>
+            </ul>
         </div>
     )
 }
