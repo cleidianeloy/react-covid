3eb47597aae0ef26c851330718d5e18f80d807e0
cleidianeloy <cleidianeloy@hotmail.com>
2022-12-29 11:43:24 -0300

    adicionando um import do Map,js e utilizando o state com os estados para modificar o Map

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@
 import {Select} from './Select';
 import {States} from './States'
 import { useState} from 'react';
+import {Map} from './Map';
+
 function App() {
   const [state, setState] = useState({});
 
 
   const changeState = (newState)=> {
     setState(newState)
-    console.log(newState)
   }
   return (
     <div >
       <h1>Situação do Covid no Brasil</h1>
       <Select onChange={changeState}/>
       <States state={state} />
+      <Map state={state} />
     </div>
   );
 }
